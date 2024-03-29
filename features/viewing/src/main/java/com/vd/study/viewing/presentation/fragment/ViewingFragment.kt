0d9de67f24d3c76cec957a29b526676b500ee2b5
Vladislav Shcherba <v.shcherba.dev@gmail.com>
2024-03-29 16:59:34 +0300

    viewing fragment was designed and gif sharing was added

--- features/viewing/src/main/java/com/vd/study/viewing/presentation/fragment/ViewingFragment.kt
+++ features/viewing/src/main/java/com/vd/study/viewing/presentation/fragment/ViewingFragment.kt
@@ -1,14 +1,19 @@
 package com.vd.study.viewing.presentation.fragment
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.vd.study.core.presentation.toast.showToast
 import com.vd.study.core.presentation.viewbinding.viewBinding
 import com.vd.study.viewing.R
@@ -18,6 +23,7 @@ import com.vd.study.viewing.domain.entities.GifEntity
 import com.vd.study.viewing.presentation.viewmodel.ViewingViewModel
 import com.vd.study.viewing.presentation.worker.GifWorker
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
 import com.vd.study.core.R as CoreResources
 
 @AndroidEntryPoint
@@ -63,7 +69,7 @@ class ViewingFragment : Fragment(R.layout.fragment_viewing) {
 
     private fun setListeners() = with(binding) {
         btnBack.setOnClickListener { viewModel.popBackStack() }
-        btnShare.setOnClickListener { shareGif() }
+        btnShare.setOnClickListener { shareGifFile() }
         btnLike.setOnClickListener { handleGifLike() }
         btnSave.setOnClickListener { handleGifSave() }
     }
@@ -104,13 +110,52 @@ class ViewingFragment : Fragment(R.layout.fragment_viewing) {
         WorkManager.getInstance(requireContext()).enqueue(updateGifWorkRequest)
     }
 
-    private fun shareGif() {
+    private fun shareGifFile() {
+        Glide.with(this)
+            .asFile()
+            .load(gif.url)
+            .into(object : CustomTarget<File>() {
+                override fun onResourceReady(resource: File, transition: Transition<in File>?) {
+                    shareGifFile(resource)
+                }
 
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    requireContext().showToast(resources.getString(CoreResources.string.error))
+                }
+            })
+    }
+
+    private fun shareGifFile(gifFile: File) {
+        val uri = FileProvider.getUriForFile(
+            requireContext(),
+            "${requireContext().packageName}.provider",
+            gifFile
+        )
+
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "image/*"
+            putExtra(Intent.EXTRA_STREAM, uri)
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.view_gif_here, gif.url))
+        }
+        startActivity(
+            Intent.createChooser(
+                shareIntent,
+                resources.getString(R.string.share_gif_image)
+            )
+        )
     }
 
     private fun loadUI() {
         viewModel.changeBottomBarTheme(false)
         loadAuthor(gif.author)
+
+        Glide.with(requireContext())
+            .load(CoreResources.drawable.giphy)
+            .placeholder(CoreResources.drawable.placeholder_gray_gradient)
+            .centerCrop()
+            .into(binding.imageGiphyProvider)
 
         Glide.with(requireContext())
             .load(gif.url)
@@ -132,13 +177,15 @@ class ViewingFragment : Fragment(R.layout.fragment_viewing) {
             binding.textAuthorName.text = resources.getString(R.string.giphy)
             Glide.with(requireContext())
                 .load(R.drawable.giphy)
+                .centerCrop()
                 .error(R.drawable.account_loading_error)
                 .into(binding.accountImage)
         } else {
             binding.textAuthorName.text = author.username
             Glide.with(requireContext())
                 .load(author.avatarUrl)
-                .placeholder(R.drawable.account_placeholder)
+                .centerCrop()
+                .placeholder(CoreResources.drawable.placeholder_gray_gradient)
                 .error(R.drawable.account_loading_error)
                 .into(binding.accountImage)
         }
@@ -148,15 +195,3 @@ class ViewingFragment : Fragment(R.layout.fragment_viewing) {
         const val INCOMING_GIF_KEY = "viewingGif"
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
